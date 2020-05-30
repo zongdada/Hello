6a4902a5539d4e78a8c225cfcdab69162f4c4995
63433 <63433>
2020-05-30 23:18:35 +0800

    测试dev分支me

--- myMoven/src/main/java/HelloGit.java
+++ myMoven/src/main/java/HelloGit.java
@@ -1,5 +1,6 @@
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("you");
+        System.out.println("dev分支me");
+        System.out.println("添加分支");
     }
 }
