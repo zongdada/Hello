a44e4bf766ee8ad7b4e54d2d94f7bcf68eae751c
63433 <63433>
2020-05-30 23:25:17 +0800

    dev分支

--- myMoven/src/main/java/HelloGit.java
+++ myMoven/src/main/java/HelloGit.java
@@ -2,5 +2,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("dev分支me");
         System.out.println("添加分支");
+        System.out.println("dev分支");
     }
 }
